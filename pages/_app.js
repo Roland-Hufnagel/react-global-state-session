2369b5a209b123a1e828d1c5fa09e920764f1ff8
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-04-11 17:14:59 +0200

    session-end

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,51 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialAnimals = [
+  { id: 1, name: "Cats", count: 0 },
+  { id: 2, name: "Dogs", count: 0 },
+  { id: 3, name: "Sheep", count: 0 },
+  { id: 4, name: "Dragons", count: 0 },
+];
 
 export default function App({ Component, pageProps }) {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  const animalCounts = animals.map((animal) => animal.count);//z.B. [4,0,3,5]
+  const countSum = animalCounts.reduce((a, b) => a + b);
+  const countAverage = countSum / animals.length;
+  const dragonCount = animals.find((animal) => animal.name === "Dragons").count;
+
+  function handleAdd(id) {
+    setAnimals(
+      animals.map((animal) =>
+        animal.id === id ? { ...animal, count: animal.count + 1 } : animal
+      )
+    );
+  }
+  function handleSubtract(id) {
+    const newArray = animals.map((animal) =>
+      animal.id === id
+        ? { ...animal, count: Math.max(0, animal.count - 1) }
+        : animal
+    );
+    setAnimals(newArray);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
+      <Layout countSum={countSum} dragonCount={dragonCount}>
+        <Component
+          {...pageProps}
+          animals={animals}
+          handleAdd={handleAdd}
+          handleSubtract={handleSubtract}
+          countSum={countSum}
+          countAverage={countAverage}
+          dragonCount={dragonCount}
+        />
       </Layout>
     </>
   );
